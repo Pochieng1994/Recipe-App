bada367bb672aec77d6b1a464bd5f084789ecb94
Peter Ogada <peterogada@peters-air-2.mynetworksettings.com>
2023-08-25 15:41:57 -0400

    Finished working on project for today pushing up to github

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const mealImage = document.querySelector('.meal-image');
-const nameOfMeal = document.querySelector('.name-of-meal');
-const ingredients = document.querySelector('.ingredients');
-const instructions = document.querySelector('.instructions');
-const youTube = document.querySelector('.youtube');
-const newRecipe = document.querySelector('.new-recipe');
+let mealImage = document.querySelector('.meal-image');
+let nameOfMeal = document.querySelector('.name-of-meal');
+let ingredients = document.querySelector('.ingredients');
+let instructions = document.querySelector('.instructions');
+let youTube = document.querySelector('.youtube');
+let newRecipe = document.querySelector('.new-recipe');
 
 function getData() {
-  axios.get('www.themealdb.com/api/json/v1/1/random.php').then(response => {
-    console.log(response);
+  axios.get('https://www.themealdb.com/api/json/v1/1/random.php').then(response => {
+    nameOfMeal.textContent = response.data.meals[0].strMeal
+    instructions.textContent = response.data.meals[0].strInstructions
   })
 }
 
+getData()
 
