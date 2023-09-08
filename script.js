f110e3c67c939867db224181746e946c5e550c26
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2023-09-07 22:20:59 -0400

    Finished working on app for the day pushing to github

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ let image = document.querySelector('.image');
 let nameOfMeal = document.querySelector('.name-of-meal');
 let ingredients = document.querySelector('.ingredients');
 let instructions = document.querySelector('.instructions');
-let youTubeButton = document.querySelector('.youtube');
+let anchorDiv =document.querySelector('.anchorDiv');
+let youtubeLink = document.createElement('a');
 let newRecipeButton = document.querySelector('.new-recipe');
+
+youtubeLink.textContent = 'Youtube Video'
+anchorDiv.appendChild(youtubeLink)
+
 
 
 function displayRecipe() {
@@ -12,7 +17,9 @@ function displayRecipe() {
     nameOfMeal.textContent = response.data.meals[0].strMeal
     image.setAttribute('src', response.data.meals[0].strMealThumb)
     instructions.textContent = response.data.meals[0].strInstructions
-    let urlData = response.data.meals[0].strYoutube
+    youtubeLink.setAttribute('href', response.data.meals[0].strYoutube)
+    youtubeLink.setAttribute('target', '_blank')
+
 
     
     let mealData = Object.entries(response.data.meals[0]) 
@@ -36,9 +43,6 @@ function displayRecipe() {
       }
     }
 
-    youTubeButton.addEventListener('click', () => {
-     window.open(urlData, '_blank')
-  })
   })
   .catch(function(error) {
     console.log(error);
@@ -58,58 +62,12 @@ displayRecipe()
 
 
 
-/*function array () {
-  axios.get('https://www.themealdb.com/api/json/v1/1/random.php').then(response => {
-    let result = Object.entries(response.data.meals[0])
-    let arrayOfIngredients = [];
-
-
-    for(let i = 0; i < result.length; i++) {
-      if([i] > 8 && [i] < 29) {
-        arrayOfIngredients.push(result[i])   
-      }
-    }
-    
-    let newArrayOfIngredients = arrayOfIngredients.map((item) => {
-      let [first, second] = item
-      return second
-    })
-
-    for(let i = 0; i < newArrayOfIngredients.length; i++) {
-      if(newArrayOfIngredients[i] === '') {
-        newArrayOfIngredients.splice(i)
-        ingredients.textContent = newArrayOfIngredients
-      }
-    }
-  })
-  
-}*/
-
-/*let ages = [1, 2, 3, 4, 5, 6, 7]; 
-let array = [];
-
-function index () {
-  for(let i = 0; i < ages.length; i++) {
-    if([i] > 0 && [i] < 4) {
-      array.push(ages[i])
-      console.log(array)
-    }
-  }
-}*/
 
 
 
 
-/*function arrayFunction() {
-  let arr = ['apple', 'pear', 'orange', 'hey',null, null]
-  let emptyString = null;
-  
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] === emptyString){ 
-     console.log('You are awesome')
-    }
-  }
-}*/
+
+
 
 
 
